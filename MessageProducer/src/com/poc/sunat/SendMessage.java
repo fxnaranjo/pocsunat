0935fa4f3e96ec8cb3917eaeb560926560b7ab86
Francisco Naranjo <fnaranjo@fnaranjo.ibm.com>
2020-12-30 19:21:10 -0500

    first commit

---
+++ MessageProducer/src/com/poc/sunat/SendMessage.java
@@ -0,0 +1,160 @@
+
+
+package com.poc.sunat;
+
+
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.TextMessage;
+
+import com.ibm.msg.client.jms.JmsConnectionFactory;
+import com.ibm.msg.client.jms.JmsFactoryFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
+
+/**
+ * A minimal and simple application for Point-to-point messaging.
+ *
+ *
+ * Notes:
+ *
+ * API type: JMS API (v2.0, simplified domain)
+ *
+ * Messaging domain: Point-to-point
+ *
+ * Provider type: IBM MQ
+ *
+ * Connection mode: Client connection
+ *
+ * JNDI in use: No
+ *
+ */
+public class SendMessage {
+
+	// System exit status value (assume unset value to be 1)
+	private static int status = 1;
+
+	// Create variables for the connection to MQ
+	private static final String HOST = "localhost"; // Host name or IP address
+	private static final int PORT = 1415; // Listener port for your queue manager
+	private static final String CHANNEL = "FXNQMGRIIB.SVRCONN"; // Channel name
+	private static final String QMGR = "FXNQMGRIIB"; // Queue manager name
+	private static final String APP_USER = "mqm"; // User name that application uses to connect to MQ
+	private static final String APP_PASSWORD = "020kw31xx"; // Password that the application uses to connect to MQ
+	private static final String QUEUE_NAME = "SUNAT_IN"; // Queue that the application uses to put and get messages to and from
+
+
+	/**
+	 * Main method
+	 *
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		// Variables
+		JMSContext context = null;
+		Destination destination = null;
+		JMSProducer producer = null;
+
+
+
+		try {
+			// Create a connection factory
+			JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
+			JmsConnectionFactory cf = ff.createConnectionFactory();
+
+			// Set the properties
+			cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, HOST);
+			cf.setIntProperty(WMQConstants.WMQ_PORT, PORT);
+			cf.setStringProperty(WMQConstants.WMQ_CHANNEL, CHANNEL);
+			cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+			cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, QMGR);
+			cf.setStringProperty(WMQConstants.WMQ_APPLICATIONNAME, "SUNAT TEST PRODUCER APP");
+			cf.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
+			cf.setStringProperty(WMQConstants.USERID, APP_USER);
+			cf.setStringProperty(WMQConstants.PASSWORD, APP_PASSWORD);
+
+			// Create JMS objects
+			context = cf.createContext();
+			destination = context.createQueue("queue:///" + QUEUE_NAME);
+			producer = context.createProducer();
+			
+			long uniqueNumber=0;
+			TextMessage message=null;
+			
+			int numMessages=5;
+			
+			numMessages = ((System.getenv("NUM_MENSAJES"))!=null ? Integer.parseInt(System.getenv("NUM_MENSAJES")):5);
+				
+			System.out.println("Num Messages="+numMessages);
+			
+			
+			do
+			{
+			
+				uniqueNumber = System.currentTimeMillis() % 1000;
+				message = context.createTextMessage("Your lucky number today is " + uniqueNumber);
+				producer.send(destination, message);
+				System.out.println("Sent message:"+numMessages);
+				numMessages=numMessages-1;
+				Thread.sleep(1500);
+			}while(numMessages!=0);
+			
+
+			recordSuccess();
+		} catch (JMSException jmsex) {
+			recordFailure(jmsex);
+		}
+
+		System.exit(status);
+
+	} // end main()
+
+	/**
+	 * Record this run as successful.
+	 */
+	private static void recordSuccess() {
+		System.out.println("SUCCESS");
+		status = 0;
+		return;
+	}
+
+	/**
+	 * Record this run as failure.
+	 *
+	 * @param ex
+	 */
+	private static void recordFailure(Exception ex) {
+		if (ex != null) {
+			if (ex instanceof JMSException) {
+				processJMSException((JMSException) ex);
+			} else {
+				System.out.println(ex);
+			}
+		}
+		System.out.println("FAILURE");
+		status = -1;
+		return;
+	}
+
+	/**
+	 * Process a JMSException and any associated inner exceptions.
+	 *
+	 * @param jmsex
+	 */
+	private static void processJMSException(JMSException jmsex) {
+		System.out.println(jmsex);
+		Throwable innerException = jmsex.getLinkedException();
+		if (innerException != null) {
+			System.out.println("Inner exception(s):");
+		}
+		while (innerException != null) {
+			System.out.println(innerException);
+			innerException = innerException.getCause();
+		}
+		return;
+	}
+
+}
